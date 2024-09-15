92ef0f1c822d4bd9d38a9dbaa37f69375bdeccb8
Parth Adhana <parth007adhana@gmail.com>
2024-09-15 22:57:15 +0530

    Added gitignore

--- Testing Round 19 (Div. 3) C1.cpp
+++ Testing Round 19 (Div. 3) C1.cpp
@@ -53,83 +53,70 @@ template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i
 */
 
 void solve()
-{
-    ll n,sm;cin>>n>>sm;
-    vector<ll>a(n);
-    vector<ll>b(n);
-    map<ll,ll>mp;
-    bool flag=0;
-    for (int i = 0; i < n; ++i)
-    {
-        cin>>a[i];
-        if(a[i]<=sm)flag=1;
-    }
-    for(int i=0;i<n;i++)
-    {
-        cin>>b[i];
-        mp[a[i]]=b[i];
-    }
-    // if(flag==0){
-    //     cout<<0<<endl;
-    //     return;
-    // }
-    ll ans=0;
-    debug(mp)
-    for(int i=0;i<n;i++)
-    {
-        ll times=b[i];
-        ll number=a[i];
-        // (k1+k2)x +k2<=sm;
-        ll mx=(mp[a[i]]+mp[a[i]+1])*a[i]+mp[a[i]+1];
-        debug(a[i])
-        debug(mx)
-        if(mx<=sm)
+{      
+   string s;cin>>s;
+   int n=s.size();
+   string pref;pref+=s[0];
+   for(int i=1;i<n;i++)
+   {
+        pref+=s[i];
+        string temp;
+        int overlap=0;
+        for(int j=i;j<i+pref.size();j++)
         {
-            
-            ans=max(ans,mx);
-            // if(mx==sm)
-            // {
-            //     debug(a[i])
-            //     debug(mx)    
-            // }
-            
-        }
-        else
-        {
-            if(mp[a[i]+1]==0)
-            {
-                ll t=sm/a[i];
-                ans=max(ans,a[i]*t);
-                continue;
+            if(j>n){
+                cout<<"NO"<<endl;
+                return;
             }
-            ll let=min(mp[a[i]],sm/a[i]);
-            debug(let)
-            ll x=sm-let*a[i];
-            x/=(a[i]+1);
-            ll temp2=(let*a[i]+x*(a[i]+1));
-            debug(x)
-            debug(temp2)
-            
-            ans=max(ans,min(sm,temp2+min(let,mp[a[i]+1]-x)));
+            temp+=s[j];
         }
-        debug(ans)
-    }
-    cout<<ans<<endl;
-    
-    
+        cout<<pref<<' '<<temp<<endl;
+        if(pref==temp)
+        {
+            bool flag=1;
+            overlap=2*temp.size()-(i+pref.size());
+            cout<<overlap<<endl;
+            string s1,s2;
+            // last overlap char should be same as first char of s
+            for(int k=temp.size()-1,it=0;it<overlap;it++,k--)
+            {
+                cout<<k<<endl;
+                if(k>n)
+                {
+                    cout<<"NO"<<endl;
+                    return;
+                }
+                s1+=s[it];
+                s2+=s[k];
+            }
+            //
+            reverse(all(s2));
+            cout<<s1<<' '<<s2<<endl;
+            if(s1!=s2)continue;
+            cout<<"YES"<<endl;
+            for(int k=i;k<n;k++)
+            {
+                cout<<s[k];
+            }
+            cout<<endl;
+            return;
+        }
+   }
+   cout<<"NO"<<endl;
+
 }
 
 int main() {    
-#ifndef ONLINE_JUDGE
-    freopen("error.txt", "w", stderr);
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+//     freopen("error.txt", "w", stderr);
+//     freopen("input.txt","r",stdin);
+//     freopen("output.txt","w",stdout);
+// #endif
     
     fastio();
-    ll T;cin>>T;
+    ll T;
+    T=1;
     while(T--){
         solve();
     }
 }
-
